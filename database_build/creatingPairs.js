2229731752557dc5ed8a2fff786c0635f736bc91
Kevin Hsieh <kevawo@gmail.com>
2021-11-23 16:26:02 -0600

    makePairs function v3

--- database_build/creatingPairs.js
+++ database_build/creatingPairs.js
@@ -1,6 +1,5 @@
-function makePairs() {
+function makeGatePairs() {
   var gates = ["A01", "A02", "A03", "A04", "A05", "B01", "B02", "B03", "B04", "B05"];
-  var airport_code = ["BKK", "ICN", "LHR", "JFK", "LAX", "MNL", "IAH", "HND", "GMP", "SEA", "SFO", "MEL", "TPE", "TOJ", "PEK"]
 
   var gates1 = gates.slice(),
     gates2 = gates.slice();
@@ -12,8 +11,15 @@ function makePairs() {
     return 0.5 - Math.random();
   });
 
-  for (var i = 0; i < gates.length; i++)
-    console.log(gates1[i], gates2[i]);
+  for (var i = 0; i < gates.length; i++) {
+    if (gates1[i] == gates2[i]) continue;
+    else console.log(gates1[i], gates2[i]);
+  }
+
+}
+
+function makeAirportPairs() {
+  var airport_code = ["BKK", "ICN", "LHR", "JFK", "LAX", "MNL", "IAH", "HND", "GMP", "SEA", "SFO", "MEL", "TPE", "TOJ", "PEK"]
 
   var air1 = airport_code.slice(),
     air2 = airport_code.slice();
@@ -24,9 +30,13 @@ function makePairs() {
   air2.sort(function () {
     return 0.5 - Math.random();
   });
-  for (var i = 0; i < airport_code.length; i++)
-    console.log(air1[i], air2[i]);
+  for (var i = 0; i < airport_code.length; i++) {
+    if (air1[i] == air2[i]) continue;
+    else console.log(air1[i], air2[i]);
+  }
+
 }
 
-makePairs();
-makePairs();
+makeAirportPairs();
+console.log("--------------------------------")
+makeGatePairs();

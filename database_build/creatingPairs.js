4b7481d2f5e0beb49c0113147e1105e679b2f4e3
Kevin Hsieh <kevawo@gmail.com>
2021-11-23 17:17:09 -0600

    Separated Arrival & Depature Pairs

--- database_build/creatingPairs.js
+++ database_build/creatingPairs.js
@@ -1,41 +1,55 @@
 function makeGatePairs() {
   var gates = ["A01", "A02", "A03", "A04", "A05", "B01", "B02", "B03", "B04", "B05"];
+  var arrivalGatesArray = [];
+  var departureGatesArray = [];
+  var depGates = gates.slice(),
+    arrGates = gates.slice();
 
-  var gates1 = gates.slice(),
-    gates2 = gates.slice();
-
-  gates1.sort(function () {
+  depGates.sort(function () {
     return 0.5 - Math.random();
   });
-  gates2.sort(function () {
+  arrGates.sort(function () {
     return 0.5 - Math.random();
   });
 
   for (var i = 0; i < gates.length; i++) {
-    if (gates1[i] == gates2[i]) continue;
-    else console.log(gates1[i], gates2[i]);
+    if (depGates[i] == arrGates[i]) continue;
+    else {
+      arrivalGatesArray.push(arrGates[i]);
+      departureGatesArray.push(depGates[i]);
+      console.log(depGates[i], arrGates[i]);
+    }
   }
-
+  console.log("departure gate:", departureGatesArray);
+  console.log("arrival gate:", arrivalGatesArray);
 }
 
 function makeAirportPairs() {
   var airport_code = ["BKK", "ICN", "LHR", "JFK", "LAX", "MNL", "IAH", "HND", "GMP", "SEA", "SFO", "MEL", "TPE", "TOJ", "PEK"]
+  var arrivalAirportArray = [];
+  var departureAirportArray = [];
+  var depAirport = airport_code.slice(),
+    arrAirport = airport_code.slice();
 
-  var air1 = airport_code.slice(),
-    air2 = airport_code.slice();
-
-  air1.sort(function () {
+  depAirport.sort(function () {
     return 0.5 - Math.random();
   });
-  air2.sort(function () {
+  arrAirport.sort(function () {
     return 0.5 - Math.random();
   });
+
   for (var i = 0; i < airport_code.length; i++) {
-    if (air1[i] == air2[i]) continue;
-    else console.log(air1[i], air2[i]);
+    if (depAirport[i] == arrAirport[i]) continue;
+    else {
+      arrivalAirportArray.push(arrAirport[i]);
+      departureAirportArray.push(depAirport[i]);
+      console.log(depAirport[i], arrAirport[i]);
+    }
   }
-
+  console.log("departure airport:", departureAirportArray);
+  console.log("arrival airport:", arrivalAirportArray);
 }
+
 
 makeAirportPairs();
 console.log("--------------------------------")

fae96b6077314bdc449933481740293aaaee392a
Gabriel Cruz <62117580+gabrielzurc10@users.noreply.github.com>
2021-11-28 22:53:57 -0600

    Server and Client created (server contains sql requests, client contains react.js)

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+require('./routes/emailLoginDisplay.js')(app);
+
+app.listen(5000, () => {
+    console.log("Server has started on port 5000");
+});

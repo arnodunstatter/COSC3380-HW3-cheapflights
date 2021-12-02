0b36e883a99792ea43a3d6a168c4ec6ca2d9bf79
Kevin Hsieh <kevawo@gmail.com>
2021-12-02 12:50:56 -0600

    Added append to query.sql

--- database_build/displayWaitlist.js
+++ database_build/displayWaitlist.js
@@ -36,17 +36,33 @@ async function main() {
 }
 
 async function displayWaitList(client, ticket_no) {
-
-    var query = await client.query(`SELECT book_ref, ticket_no, passport_no, flight_no FROM passengers LEFT JOIN tickets USING(passport_no) WHERE ticket_no = ${ticket_no}`)
+    async function clientQueryAndWriteToQuerySQL(client, transactionStr)
+    {
+        fs.appendFileSync("query.sql", transactionStr+"\r", function (err) {
+            console.log(err);
+        });
+        return await client.query(transactionStr);
+    }
+    
+    var query = await clientQueryAndWriteToQuerySQL(client,`\r\r
+SELECT book_ref, ticket_no, passport_no, flight_no 
+FROM passengers LEFT JOIN tickets USING(passport_no) 
+WHERE ticket_no = ${ticket_no}`)
     var flight_no = query.rows[0]["flight_no"];
     console.log(query.rows)
 
 
-    var economy_waitlist_query = await client.query(`select * from economy_waitlist where flight_no = ${flight_no};`);
+    var economy_waitlist_query = await clientQueryAndWriteToQuerySQL(client,`\r\r
+SELECT * 
+FROM economy_waitlist 
+WHERE flight_no = ${flight_no};`);
     var economy_waitlist = economy_waitlist_query.rows;
     console.log(economy_waitlist);
 
-    var business_waitlist_query = await client.query(`select * from business_waitlist where flight_no = ${flight_no};`);
+    var business_waitlist_query = await clientQueryAndWriteToQuerySQL(client,`\r\r
+SELECT * 
+FROM business_waitlist 
+WHERE flight_no = ${flight_no};`);
     var business_waitlist = business_waitlist_query.rows;
     console.log(business_waitlist);
 

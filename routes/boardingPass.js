f4b135575b804a2c7ae82c2f9933b9aa6a8bebe7
Kevin Hsieh <kevawo@gmail.com>
2021-12-02 21:24:51 -0600

    Fixed something

--- routes/boardingPass.js
+++ routes/boardingPass.js
@@ -35,16 +35,12 @@ module.exports = app => {
         //**generate boarding_pass */
         async function getBoardingPass(client, ticket_no) {
 
-            const str = 'getBoardingPass(client,ticket_no)';
-            const file = 'query.sql';
 
             try {
 
-
-
                 async function clientQueryAndWriteToQuerySQL(client, transactionStr)
                 {
-                    fs.appendFileSync("query.sql", transactionStr+`\r\r--The following sql statements are part of the query for for ${str}\r`, function (err) {
+                    fs.appendFileSync("query.sql", transactionStr+`\r\r--The following sql statements are part of the query for for getBoardingPass(client,${ticket_no})\r`, function (err) {
                         console.log(err);
                     });
                     return await client.query(transactionStr);

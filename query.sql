b32be298423e3fec0d48934dd75091ccbc289556
melika nassizadeh <Melikanass@gmail.com>
2021-12-08 02:44:27 -0600

    :')

--- query.sql
+++ query.sql
@@ -1,5 +1,3 @@
-
-
 --The following sql statements are part of the query for findFlights(client, 2021-12-01, Houston, Madrid)
 
 
@@ -54,3 +52,162 @@ LEFT JOIN airport_cities AS dep ON dep.airport_code = f.departure_airport_code
 LEFT JOIN passengers AS p USING(passport_no)
 LEFT JOIN aircraft AS plane USING(aircraft_code)
 WHERE email_address = 'james_jameson@hotmail.org';
+
+
+
+
+--The following sql statements are part of the query for findFlight(client, 2021-12-9, Houston, Madrid, 1, economy)
+
+SELECT  f.flight_no,
+        f.departure_time, 
+        f.arrival_time, 
+        f.econ_price                              AS price,
+        f.available_economy_seats                    AS available_seats,
+        AGE( f.arrival_time, f.departure_time)  AS elapsedTime, 
+        0                                       AS stop,
+        d.city_name                             AS departureCity, 
+        f.departure_airport_code                AS departureAirPortCode,
+        a.city_name                             AS arrivalCity, 
+        f.arrival_airport_code                  AS arrivalAirPortCode,
+        NULL                                    AS flight_no2, 
+        NULL                                    AS departure_time2, 
+        NULL                                    AS arrival_time2,
+        NULL                                    AS price2,
+        NULL                                    AS available_seats2,
+        NULL                                    AS elapsedTime2,
+        AGE(f.arrival_time, f.departure_time)   AS totalElapsedTime,
+        NULL                                    AS departureCity2, 
+        NULL                                    AS departureAirPortCode2,
+        NULL                                    AS arrivalCity2, 
+        NULL                                    AS arrivalAirPortCode2
+FROM flights                                    AS f
+        INNER JOIN airport_cities               AS d 
+            ON f.departure_airport_code = d.airport_code
+        INNER JOIN airport_cities               AS a 
+            ON f.arrival_airport_code = a.airport_code
+WHERE d.city_name = 'Houston' 
+                AND a.city_name = 'Madrid'
+                AND DATE(f.departure_time) = '2021-12-9'
+                AND f.available_economy_seats >= '1'
+                
+UNION ALL
+
+SELECT  f1.flight_no, 
+        f1.departure_time, 
+        f1.arrival_time, 
+        f1.econ_price                                 AS price,
+        f1.available_economy_seats                       AS available_seats,
+        AGE(f1.arrival_time, f1.departure_time)     AS totalElapsedTime, 
+        1                                           AS stop,
+        d.city_name                                 AS departureCity, 
+        f1.departure_airport_code                   AS departureAirPortCode,
+        m.city_name                                 AS arrivalCity, 
+        f1.arrival_airport_code                     AS arrivalAirPortCode,
+        f2.flight_no, 
+        f2.departure_time, 
+        f2.arrival_time, 
+        f2.econ_price                                 AS price2,
+        f2.available_economy_seats                       AS available_seats2,
+        AGE(f2.arrival_time, f2.departure_time)     AS elapsedTime2,
+        AGE(f2.arrival_time, f1.departure_time)     AS totalElapsedTime,
+        m.city_name                                 AS departureCity2, 
+        f2.departure_airport_code                   AS departureAirPortCode2,
+        a.city_name                                 AS arrivalCity2, 
+        f2.arrival_airport_code                     AS arrivalAirPortCode2
+FROM flights AS f1
+        INNER JOIN flights                          AS f2 
+            ON f1.arrival_airport_code = f2.departure_airport_code 
+                AND f1.departure_airport_code != f2.departure_airport_code 
+        INNER JOIN airport_cities                   AS d 
+            ON f1.departure_airport_code = d.airport_code
+        INNER JOIN airport_cities                   AS a 
+            ON f2.arrival_airport_code = a.airport_code
+        INNER JOIN airport_cities                   AS m 
+            ON f2.departure_airport_code = m.airport_code
+WHERE d.city_name = 'Houston' 
+                AND a.city_name = 'Madrid' 
+                AND f2.departure_time > (f1.arrival_time + INTERVAL '1 Hour') 
+                AND f2.departure_time < (f1.arrival_time + INTERVAL '6 Hour') 
+                AND DATE(f1.departure_time) = '2021-12-9' 
+                AND (f1.available_economy_seats) >= '1' 
+                AND (f2.available_economy_seats) >= '1' 
+    ORDER BY 7,2 ASC
+    LIMIT 20;
+
+
+--The following sql statements are part of the query for findFlight(client, 2021-12-10, Madrid, Houston, 1, economy)
+SELECT  f.flight_no,
+        f.departure_time, 
+        f.arrival_time, 
+        f.econ_price                              AS price,
+        f.available_economy_seats                    AS available_seats,
+        AGE( f.arrival_time, f.departure_time)  AS elapsedTime, 
+        0                                       AS stop,
+        d.city_name                             AS departureCity, 
+        f.departure_airport_code                AS departureAirPortCode,
+        a.city_name                             AS arrivalCity, 
+        f.arrival_airport_code                  AS arrivalAirPortCode,
+        NULL                                    AS flight_no2, 
+        NULL                                    AS departure_time2, 
+        NULL                                    AS arrival_time2,
+        NULL                                    AS price2,
+        NULL                                    AS available_seats2,
+        NULL                                    AS elapsedTime2,
+        AGE(f.arrival_time, f.departure_time)   AS totalElapsedTime,
+        NULL                                    AS departureCity2, 
+        NULL                                    AS departureAirPortCode2,
+        NULL                                    AS arrivalCity2, 
+        NULL                                    AS arrivalAirPortCode2
+FROM flights                                    AS f
+        INNER JOIN airport_cities               AS d 
+            ON f.departure_airport_code = d.airport_code
+        INNER JOIN airport_cities               AS a 
+            ON f.arrival_airport_code = a.airport_code
+WHERE d.city_name = 'Madrid' 
+                AND a.city_name = 'Houston'
+                AND DATE(f.departure_time) = '2021-12-10'
+                AND f.available_economy_seats >= '1'
+                
+UNION ALL
+
+SELECT  f1.flight_no, 
+        f1.departure_time, 
+        f1.arrival_time, 
+        f1.econ_price                                 AS price,
+        f1.available_economy_seats                       AS available_seats,
+        AGE(f1.arrival_time, f1.departure_time)     AS totalElapsedTime, 
+        1                                           AS stop,
+        d.city_name                                 AS departureCity, 
+        f1.departure_airport_code                   AS departureAirPortCode,
+        m.city_name                                 AS arrivalCity, 
+        f1.arrival_airport_code                     AS arrivalAirPortCode,
+        f2.flight_no, 
+        f2.departure_time, 
+        f2.arrival_time, 
+        f2.econ_price                                 AS price2,
+        f2.available_economy_seats                       AS available_seats2,
+        AGE(f2.arrival_time, f2.departure_time)     AS elapsedTime2,
+        AGE(f2.arrival_time, f1.departure_time)     AS totalElapsedTime,
+        m.city_name                                 AS departureCity2, 
+        f2.departure_airport_code                   AS departureAirPortCode2,
+        a.city_name                                 AS arrivalCity2, 
+        f2.arrival_airport_code                     AS arrivalAirPortCode2
+FROM flights AS f1
+        INNER JOIN flights                          AS f2 
+            ON f1.arrival_airport_code = f2.departure_airport_code 
+                AND f1.departure_airport_code != f2.departure_airport_code 
+        INNER JOIN airport_cities                   AS d 
+            ON f1.departure_airport_code = d.airport_code
+        INNER JOIN airport_cities                   AS a 
+            ON f2.arrival_airport_code = a.airport_code
+        INNER JOIN airport_cities                   AS m 
+            ON f2.departure_airport_code = m.airport_code
+WHERE d.city_name = 'Madrid' 
+                AND a.city_name = 'Houston' 
+                AND f2.departure_time > (f1.arrival_time + INTERVAL '1 Hour') 
+                AND f2.departure_time < (f1.arrival_time + INTERVAL '6 Hour') 
+                AND DATE(f1.departure_time) = '2021-12-10' 
+                AND (f1.available_economy_seats) >= '1' 
+                AND (f2.available_economy_seats) >= '1' 
+    ORDER BY 7,2 ASC
+    LIMIT 20;
--- transaction.sql
+++ transaction.sql
@@ -241,3 +241,24 @@ SELECT MAX(position)
 INSERT INTO economy_waitlist
     VALUES (11, 4919, '938076846');
 COMMIT;
+
+
+--The following sql statements are part of the transaction for makeBooking(client, 5444,5777,, 1, 0, none, 9876536709, 984367864,Mel,Anon,m@gmail.com,832567889,1990-12-19,economy)
+BEGIN;
+SELECT available_economy_seats, available_business_seats
+    FROM flights
+    WHERE flight_no = 5444;
+UPDATE flights
+    SET available_economy_seats = available_economy_seats - 1
+    WHERE flight_no = 5444;
+SELECT available_economy_seats, available_business_seats
+    FROM flights
+    WHERE flight_no = 5777;
+UPDATE flights
+    SET available_economy_seats = available_economy_seats - 1
+    WHERE flight_no = 5777;
+SELECT available_economy_seats, available_business_seats
+    FROM flights
+    WHERE flight_no = null;
+ROLLBACK;
+
